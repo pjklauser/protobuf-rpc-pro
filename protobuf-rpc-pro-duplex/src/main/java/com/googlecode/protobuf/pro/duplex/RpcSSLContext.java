3250b64eba059962a3f86d1d1514c973e9f882ae
pjklauser@gmail.com <pjklauser@gmail.com@72a4fda7-9617-578f-d15c-b12ae6914984>
2012-11-26 06:46:16 +0000

    update copyright notice.

--- protobuf-rpc-pro-duplex/src/main/java/com/googlecode/protobuf/pro/duplex/RpcSSLContext.java
+++ protobuf-rpc-pro-duplex/src/main/java/com/googlecode/protobuf/pro/duplex/RpcSSLContext.java
@@ -1,5 +1,5 @@
 /**
- *   Copyright 2010 Peter Klauser
+ *   Copyright 2010-2013 Peter Klauser
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
- */
+*/
 package com.googlecode.protobuf.pro.duplex;
 
 import java.io.IOException;
