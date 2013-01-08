6fe951c1ea7b2800e9bb4d470f40e737249e5323
pjklauser@gmail.com <pjklauser@gmail.com@72a4fda7-9617-578f-d15c-b12ae6914984>
2013-01-08 18:12:49 +0000

    Issue 17: test of getting Netty ChannelPipeline from RpcClient.

---
+++ trunk/protobuf-rpc-pro-demo/src/main/java/com/googlecode/protobuf/pro/duplex/example/execution/PipelineModifyingClient.java
@@ -0,0 +1,50 @@
+/**
+ *   Copyright 2010-2013 Peter Klauser
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+*/
+package com.googlecode.protobuf.pro.duplex.example.execution;
+
+import org.jboss.netty.channel.ChannelPipeline;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.googlecode.protobuf.pro.duplex.RpcClientChannel;
+import com.googlecode.protobuf.pro.duplex.example.wire.ExecutableClient;
+
+public class PipelineModifyingClient implements ExecutableClient {
+
+	private static Logger log = LoggerFactory.getLogger(PipelineModifyingClient.class);
+
+	private Throwable error;
+	
+	public PipelineModifyingClient() {
+	}
+	
+	@Override
+	public void execute(RpcClientChannel channel) {
+		try {
+			ChannelPipeline p = channel.getPipeline();
+			log.info("ChannelPipeline handlers: " + p.getNames());
+			
+		} catch ( Throwable t ) {
+			this.error = t;
+		}
+	}
+
+	@Override
+	public Throwable getError() {
+		return this.error;
+	}
+
+}
