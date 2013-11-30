17c1c7ce027d0377bd430a029304ff61c7c9d6cb
pjklauser@gmail.com <pjklauser@gmail.com@72a4fda7-9617-578f-d15c-b12ae6914984>
2013-11-30 15:56:52 +0000

    ssl keys which are valid for PKIX cert chain validation.

--- protobuf-rpc-pro-duplex/ssl/RunKeyTool.cmd
+++ protobuf-rpc-pro-duplex/ssl/RunKeyTool.cmd
@@ -1,3 +1,3 @@
-java -classpath ..\target\protobuf-rpc-pro-duplex-1.2.0.jar com.googlecode.protobuf.pro.duplex.util.KeyStoreImportUtil %1 %2 %3 %4 %5
+java -classpath ..\target\protobuf-rpc-pro-duplex-3.0.8.jar com.googlecode.protobuf.pro.duplex.util.KeyStoreImportUtil %1 %2 %3 %4 %5
 
 
