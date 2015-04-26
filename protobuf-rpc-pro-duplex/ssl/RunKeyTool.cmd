0492de4d28d5bab251e9e00f0d14d82898261143
pjklauser <pjklauser@gmail.com>
2015-04-26 18:47:08 +0200

    upgraded SSL certificates for successful build.

--- protobuf-rpc-pro-duplex/ssl/RunKeyTool.cmd
+++ protobuf-rpc-pro-duplex/ssl/RunKeyTool.cmd
@@ -1,3 +1,3 @@
-java -classpath ..\target\protobuf-rpc-pro-duplex-3.0.8.jar com.googlecode.protobuf.pro.duplex.util.KeyStoreImportUtil %1 %2 %3 %4 %5
+java -classpath ..\target\protobuf-rpc-pro-duplex-3.3.jar com.googlecode.protobuf.pro.duplex.util.KeyStoreImportUtil %1 %2 %3 %4 %5
 
 
